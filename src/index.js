c335724be129b0d9d554d2941d437d87f86f047f
alwAlone <gidm.teacher@gmail.com>
2019-03-24 13:08:11 +0300

    master/feat: add advanced-zeros code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(number, base){
+  let zeroCount = number;
+  let timeBase = base;
+
+  for (let i = 2; i <= base; i++) {
+    if (timeBase % i === 0) {
+      let counter = 0;
+
+      while (timeBase % i === 0) {
+        timeBase = Math.floor(timeBase / i);
+        counter++;
+      };
+
+      let count = 0;
+      let tempNum = number;
+
+      while (tempNum / i > 0) {
+        count += Math.floor(tempNum / i);
+        tempNum = Math.floor(tempNum / i);
+      };
+      
+      let timeVar = Math.floor(count / counter);
+
+      if (zeroCount > timeVar) {
+        zeroCount = timeVar;
+      }
+    }
+  }
+  return zeroCount;
 }
